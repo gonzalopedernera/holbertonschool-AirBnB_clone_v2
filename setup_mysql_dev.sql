5ca7204d0a5d6ffa10c9ea35e325ee4e2219498b
Diego-Bonora <bonoradi@gmail.com>
2023-08-01 10:50:25 -0700

    File that create the main database and the user with the privileges

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- File that create the main database and the user with the privileges
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost";
+SET PASSWORD FOR "hbnb_dev"@"localhost" = "hbnb_dev_pwd";
+GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
