463094a7fac205203bb774d03e73a80b08d87088
Diego-Bonora <bonoradi@gmail.com>
2023-08-03 11:18:18 -0700

    fixed the do_all function to work with the db and filestorage

--- console.py
+++ console.py
@@ -230,11 +230,12 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            to_print = storage.all(HBNBCommand.classes[args])
+            for k, v in to_print.items():
+                print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            to_print = storage.all()
+            for k, v in to_print.items():
                 print_list.append(str(v))
 
         print(print_list)
--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,7 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        self.__dict__.pop('_sa_instance_state', None)
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
@@ -39,14 +40,12 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
+        dictionary = self.__dict__.copy()
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if dictionary["_sa_instance_state"]:
-            del dictionary["_sa_instance_state"]
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
