65593805ec5e7d6fab75f0e6eaa2bf40d7b0aaa0
root <gonzalopeder2@gmail.com>
2023-08-03 06:56:26 -0700

    State class update

--- models/state.py
+++ models/state.py
@@ -3,9 +3,25 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
+import os
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        from models.engine.db_storage import DBStorage
+        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            cities = []
+            for key, value in storage.__objects.items():
+                l = key.split('.')
+                if l[0] == 'City':
+                    cities.append(value)
+            filtered_cities = list(filter(lambda x: x.state_id == self.id), cities)
+            return filtered_cities
+
+
