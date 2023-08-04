5d5c8d498de49ed2db5c79f659e71a6e40f93244
Diego-Bonora <bonoradi@gmail.com>
2023-08-04 12:33:51 -0700

    modified the state test to verify if the sate saves correctly

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,18 +2,26 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+from models import storage
+import MySQLdb
 
 
 class test_state(test_basemodel):
     """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+    def setUp(self):
+        """ Set up test environment """
+        db = MySQLdb.connect(host="localhost",
+                             database="hbnb_dev_db",
+                             user="hbnb_dev",
+                             password="hbnb_dev_pwd")
+        self.cursor = db.cursor()
 
     def test_name3(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        """ check for state creation """
+        new = State(name="Alabama")
+        new.save()
+        storage.save()
+        relational = self.cursor.execute(
+            "SELECT * FROM states WHERE id = %s", (new.id))
+        self.assertEqual(relational == new)
