8a0b178edc1542475e071aee19e50e277348d7b4
gonzalopedernera <gonzalopeder2@gmail.com>
2023-08-15 13:46:40 -0700

    Task 5 completed, working and Pycodestyle corrected

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Function for display on web start"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Function for display on web hbnb route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """Function for display on web c/<text> route"""
+    formated_text = text.replace("_", " ")
+    return f"C {formated_text}"
+
+
+@app.route("pyhton/<text>", strict_slashes=False)
+def python_is_fun(text="is cool"):
+    """Function for display on web python/<text> route"""
+    if text:
+        formated_text = text.replace("_", " ")
+        return f"Python {formated_text}"
+    else:
+        return f"Pyhton {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Function to display on web app number/<n> only if n is an int"""
+    if n:
+        return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Function to display template on web app number_template/<n>"""
+    if n:
+        return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
