2db36cb03443d3a365a266599513a22465910c05
root <gonzalopeder2@gmail.com>
2023-08-16 06:28:45 -0700

    Task 3 correction

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,13 +23,13 @@ def c_is_fun(text):
     formated_text = text.replace("_", " ")
     return f"C {formated_text}"
 
-@app.route("/python/", defaults={'text': 'is cool'} )
+
+@app.route("/python/", defaults={'text': 'is cool'})
 @app.route("/pyhton/<text>", strict_slashes=False)
 def python_is_fun(text):
     """Function for display on web python/<text> route"""
-    if text:
-        formated_text = text.replace("_", " ")
-        return f"Python {formated_text}"
+    formated_text = text.replace("_", " ")
+    return f"Python {formated_text}"
 
 
 if __name__ == '__main__':
