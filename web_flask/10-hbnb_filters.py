4621d379d003ac2d9e0fffb2f801f6722fa18752
gonzalopedernera <gonzalopeder2@gmail.com>
2023-08-19 21:37:17 -0700

    Task 10, still work to do

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove_session(e):
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template(
+            '10-hbnb_filters.html',
+            states=states,
+            amenities=amenities
+            )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
