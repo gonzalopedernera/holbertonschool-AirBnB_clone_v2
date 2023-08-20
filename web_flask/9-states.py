df7cbd84b2ad1018671bfd2f2a75a54e5639d02c
gonzalopedernera <gonzalopeder2@gmail.com>
2023-08-19 20:08:49 -0700

    Task 10 working

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -13,11 +13,20 @@ def remove_session(e):
 
 
 @app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states(id=None):
     """Function to display on web app /states route"""
-    states_list = storage.all(State)
-    return render_template('9-states.html', states=states_list, id=id)
+    states_list = storage.all(State).values()
+    return render_template('7-states_list.html', states=states_list, id=id)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_found(id):
+    states_list = storage.all(State).values()
+    for state in states_list:
+        if state.id == id:
+            return render_template('9-states.html', state=state)
+    return render_template('9-states.html', state=None)
 
 
 if __name__ == '__main__':
